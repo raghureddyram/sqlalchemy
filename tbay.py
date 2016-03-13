d065d91b4efe7e12373f062b63144aae7ca6f4d6
Raghu Reddy <raghureddy.ram@gmail.com>
2016-03-13 07:52:29 +0000

    Rename to singular table names

--- tbay.py
+++ tbay.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 
 
 
@@ -12,24 +12,36 @@ session = Session()
 Base = declarative_base()
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "item"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
 
+    owner_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    items = relationship("Item", backref="owner") ##if uselist=False ommitted then will be one-to-many
 
 class Bid(Base):
-    __tablename__ = "bids"
+    __tablename__ = "bid"
 
     id = Column(Integer, primary_key=True)
-    price = Column(Float(scale=2), nullable=False) ## is this the correct syntax for currency values?
+    price = Column(Float(scale=2), nullable=False)
 
 Base.metadata.create_all(engine)
+
+raghu = User(username="raghureddyram", password="hellohello")
+steven = User(username="steven", password="smarty")
+# session.add_all([raghu, steven])
+# session.commit()
+keyring = Item(name="keyring", description="place to hold keys", owner_id=raghu.id)
+
+# session.add(keyring)
+# session.commit()
